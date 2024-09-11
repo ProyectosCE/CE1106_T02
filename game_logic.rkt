cee86057816801852a34ca5758b3517ba79b12a5
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2024-09-11 14:41:06 -0600

    [UPDATE] Algoritmo Greedy

--- game_logic.rkt
+++ game_logic.rkt
@@ -3,6 +3,36 @@
 (provide (all-defined-out))
 
 #|
+
+Funciones esenciales para el juego
+
+|#
+
+; Crea una lista de longitud n con un valor dado
+(define (get-list n value)
+  (cond ((< n 1) '())
+        (else (cons value (get-list (- n 1) value)))))
+
+; Crear una matriz de filas y columnas dadas con un valor dado
+(define (get-matrix rows columns value)
+  (cond ((< rows 1) '())
+        (else (cons (get-list columns value) (get-matrix (- rows 1) columns value)))))
+
+; Retorna la lista con el valor establecido en el índice dado
+(define (list_set_at list index value)
+  (cond ((null? list) '())
+        ((equal? index 0) (cons value (list_set_at (cdr list) (- index 1) value)))
+        (else (cons (car list) (list_set_at (cdr list) (- index 1) value)))))
+
+; Funcion para actualizar una lista en un índice específico
+(define (update-list lst index value)
+  (append (take lst index) (list value) (drop lst (+ index 1))))
+
+; Fincion para establecer un valor en la matriz
+(define (matrix-set-at matrix row column value)
+  (update-list matrix row (update-list (list-ref matrix row) column value)))
+
+#|
 =============================================================================================
 
 Condiciones de victoria del juego
@@ -161,3 +191,121 @@ Algortimo Codicioso
 =================================================================================================
 |# 
 
+; Función para obtener todas las posiciones vacías
+; 1. Conjunto de Candidatos: Obtiene todas las posiciones vacías en la matriz donde el bot puede realizar un movimiento.
+(define (empty-positions matrix)
+  (define (empty-positions-row row r-index)
+    (define (check-pos lst col-index)
+      (cond ((null? lst) '())
+            ((equal? (car lst) '_)
+             (cons (list r-index col-index)
+                   (check-pos (cdr lst) (+ col-index 1))))
+            (else (check-pos (cdr lst) (+ col-index 1))))) ; No está vacío, avanzar
+    (check-pos row 0))
+
+  (define (check-rows mat r-index)
+    (cond ((null? mat) '())
+          (else (append (empty-positions-row (car mat) r-index)
+                        (check-rows (cdr mat) (+ r-index 1)))))) ; Buscar en cada fila
+  (check-rows matrix 0))
+
+; Función para verificar si un movimiento lleva a una victoria
+; 4. Función Objetivo: Asigna un valor a una solución o solución parcial, indicando su calidad.
+(define (winning-move? matrix row col player)
+  (define (check-line line player)
+    (cond ((< (length line) 3) #f)
+          ((and (equal? (car line) player)
+                (equal? (cadr line) player)
+                (equal? (caddr line) player))
+           #t)
+          (else (check-line (cdr line) player))))
+
+  ; Chequea fila
+  (define (check-horizontal)
+    (check-line (take-line (list-ref matrix row) 0) player))
+  
+  ; Chequea columna
+  (define (check-vertical)
+    (check-line (column-line matrix col 0) player))
+  
+  ; Chequea diagonales
+  (define (check-diagonals)
+    (or (check-line (main-diagonal matrix (min (length matrix) (length (car matrix))) 0 0) player)
+        (check-line (anti-diagonal matrix (min (length matrix) (length (car matrix))) 0 (- (length (car matrix)) 1)) player)))
+  
+  (or (check-horizontal) (check-vertical) (check-diagonals)))
+
+; Función auxiliar para obtener una línea horizontal
+; 5. Función de Solución: Define cuándo se ha alcanzado una solución completa, en este caso, cuando se selecciona un movimiento que lleva a la victoria.
+(define (take-line line start)
+  (if (< (length line) 3)
+      '()
+      (cons (car line) (take-line (cdr line) (+ start 1)))))
+
+; Función auxiliar para obtener una línea vertical
+; 5. Función de Solución: Define cuándo se ha alcanzado una solución completa, en este caso, cuando se selecciona un movimiento que lleva a la victoria.
+(define (column-line matrix col row)
+  (if (>= row (length matrix))
+      '()
+      (cons (list-ref (list-ref matrix row) col) (column-line matrix col (+ row 1)))))
+
+; Función auxiliar para obtener la diagonal principal
+; 5. Función de Solución: Define cuándo se ha alcanzado una solución completa, en este caso, cuando se selecciona un movimiento que lleva a la victoria.
+(define (main-diagonal matrix length row col)
+  (if (or (>= row length) (>= col length))
+      '()
+      (cons (list-ref (list-ref matrix row) col) 
+            (main-diagonal matrix length (+ row 1) (+ col 1)))))
+
+; Función auxiliar para obtener la diagonal inversa
+; 5. Función de Solución: Define cuándo se ha alcanzado una solución completa, en este caso, cuando se selecciona un movimiento que lleva a la victoria.
+(define (anti-diagonal matrix length row col)
+  (if (or (>= row length) (< col 0))
+      '()
+      (cons (list-ref (list-ref matrix row) col)
+            (anti-diagonal matrix length (+ row 1) (- col 1)))))
+
+
+; Función para seleccionar una posición aleatoria de la lista
+; 2. Función de Selección: Selecciona un movimiento aleatorio de la lista de posiciones vacías si no se encuentran movimientos ganadores ni de bloqueo.
+(define (random-move positions)
+  (if (null? positions)
+      '()
+      (list-ref positions (random (length positions))))) ; Selecciona una posición aleatoria
+
+; Función principal que selecciona el mejor movimiento para el bot
+(define (greedy-bot matrix player)
+  (define opponent (if (equal? player 'o) 'x 'o)) ; Determina el oponente
+  
+  ; Evaluar si el bot puede ganar (Función de Selección)
+  (define (find-winning-move positions)
+    (cond
+      ((null? positions) '()) ; No hay más posiciones para evaluar
+      (else
+       (define pos (car positions))
+       (define row (car pos))
+       (define col (cadr pos))
+       (if (winning-move? (matrix-set-at matrix row col player) row col player) ; Si es un movimiento ganador
+           (list row col) ; Retorna el movimiento ganador
+           (find-winning-move (cdr positions)))))) ; Evaluar siguiente candidato
+  
+  ; Evaluar si el oponente puede ganar y necesita ser bloqueado (Función de Selección)
+  (define (find-blocking-move positions)
+    (cond
+      ((null? positions) '()) ; No hay más posiciones para evaluar
+      (else
+       (define pos (car positions))
+       (define row (car pos))
+       (define col (cadr pos))
+       (if (winning-move? (matrix-set-at matrix row col opponent) row col opponent) ; Si es un movimiento del oponente que necesita bloqueo
+           (list row col) ; Retorna el movimiento para bloquear
+           (find-blocking-move (cdr positions)))))) ; Evaluar siguiente candidato
+  
+  (define positions (empty-positions matrix)) ; Obtiene todas las posiciones vacías (Conjunto de Candidatos)
+  
+  ; Selecciona un movimiento aleatorio si es el primer movimiento (Función de Solución)
+  (if (null? (find-winning-move positions))
+      (if (null? (find-blocking-move positions))
+          (random-move positions) ; Primer movimiento vacío
+          (find-blocking-move positions)) ; Bloqueo
+      (find-winning-move positions))) ; Movimiento ganador
--- gui.rkt
+++ gui.rkt
@@ -2,22 +2,6 @@
 
 (require "game_logic.rkt")
 
-; Crea una lista de longitud n con un valor dado
-(define (get-list n value)
-  (cond ((< n 1) '())
-        (else (cons value (get-list (- n 1) value)))))
-
-; Crear una matriz de filas y columnas dadas con un valor dado
-(define (get-matrix rows columns value)
-  (cond ((< rows 1) '())
-        (else (cons (get-list columns value) (get-matrix (- rows 1) columns value)))))
-
-; Retorna la lista con el valor establecido en el índice dado
-(define (list_set_at list index value)
-  (cond ((null? list) '())
-        ((equal? index 0) (cons value (list_set_at (cdr list) (- index 1) value)))
-        (else (cons (car list) (list_set_at (cdr list) (- index 1) value)))))
-
 (define global-columns 3)
 (define global-rows 3)
 (define blue (make-object color% 0 0 255))
@@ -26,14 +10,6 @@
 
 (define current-row 0)
 (define current-column 0)
-
-; Funcion para actualizar una lista en un índice específico
-(define (update-list lst index value)
-  (append (take lst index) (list value) (drop lst (+ index 1))))
-
-; Fincion para establecer un valor en la matriz
-(define (matrix-set-at matrix row column value)
-  (update-list matrix row (update-list (list-ref matrix row) column value)))
 
 ; Ventana para jugar el juego
 (define game-frame (new frame%
@@ -50,60 +26,53 @@
                 [color blue])
     (inherit get-dc)
 
-    ; Nagevación con teclado y selección
     (define/override (on-char e)
-  (define key (send e get-key-code))  
+      (define key (send e get-key-code))
 
-  ; Debug: Imprime la tecla presionada
-  ;(displayln (format "Key pressed: ~a" key))
+      ; Verifica las teclas de flecha y la tecla 'q' para la selección
+      (cond
+        [(equal? key #\w) (set! current-row (max 0 (- current-row 1))) ]
+        [(equal? key #\s) (set! current-row (min (- global-rows 1) (+ current-row 1))) ]
+        [(equal? key #\a) (set! current-column (max 0 (- current-column 1))) ]
+        [(equal? key #\d) (set! current-column (min (- global-columns 1) (+ current-column 1))) ]
+        [(equal? key #\k)
+        ; Solo marca la celda si está vacía
+        (when (cell-empty? current-row current-column)
+          (set! game-grid (matrix-set-at game-grid current-row current-column 'x))
+          ; Actualiza el carácter mostrado en el canvas seleccionado
+          (send (list-ref canvases (+ current-column (* current-row global-columns))) set-character #\x)
+          (send (list-ref canvases (+ current-column (* current-row global-columns))) refresh)
+          (displayln game-grid)
+          
+          ; Verifica si el jugador ganó o si hay un empate después del movimiento del jugador
+          (when (draw? game-grid)
+            (message-box "Draw" "It's a draw!")
+            (reset-game))
 
-  ; Verifica las teclas de flecha y la tecla 'q' para la selección
-  (cond
-    [(equal? key #\w) (set! current-row (max 0 (- current-row 1)))]
-    [(equal? key #\s) (set! current-row (min (- global-rows 1) (+ current-row 1)))]
-    [(equal? key #\a) (set! current-column (max 0 (- current-column 1)))]
-    [(equal? key #\d) (set! current-column (min (- global-columns 1) (+ current-column 1)))]
-    [(equal? key #\k)
-     ; Marca la celda actual con 'x' independientemente de su estado anterior
-     (set! game-grid (matrix-set-at game-grid current-row current-column 'x))
-     ; Actualiza el carácter mostrado en el canvas seleccionado
-     (send (list-ref canvases (+ current-column (* current-row global-columns))) set-character #\x)
-     (send (list-ref canvases (+ current-column (* current-row global-columns))) refresh)
-     (displayln game-grid)
+          (when (winner? game-grid)
+            (message-box "Win" "You won!")
+            (reset-game))
 
+          ; Turno del computador, solo si el juego no ha terminado
+          (when (and (not (draw? game-grid)) (not (winner? game-grid)))
+            (let ((move (greedy-bot game-grid 'o))) ; Usa greedy-bot para obtener el movimiento
+              ; Establece el movimiento del computador y refresca el canvas
+              (set-computer-move move)))
 
-    ; ======================================== CAMBIAR ========================================
-     ; Verifica si el jugador ganó o si hay un empate después del movimiento del jugador
-     (when (draw? game-grid)
-       (message-box "Draw" "It's a draw!")
-       (reset-game))
+          ; Verifica otra vez si el jugador ganó después del movimiento del computador
+          (when (winner? game-grid)
+            (message-box "Win" "The computer won!")
+            (reset-game))
 
-     (when (winner? game-grid)
-       (message-box "Win" "You won!")
-       (reset-game))])
+          ; Verifica si hay un empate después del movimiento del computador
+          (when (draw? game-grid)
+            (message-box "Draw" "It's a draw!")
+            (reset-game)))])
 
-#|
-     ; Turno del computador, solo si el juego no ha terminado
-     (when (and (not (draw? game-grid)) (not (winner? game-grid)))
-       (let ((move (get-computer-next-move game-grid global-rows global-columns)))
-         ;; Set computer move and refresh the canvas
-         (set-computer-move move)))
+      ; Actualiza la pantalla para mostrar la selección actual
+      (send game-frame refresh)
+      (send this refresh))
 
-     ; Verifica otra vez si el jugador ganó después del movimiento del computador
-     (when (winner? game-grid)
-       (message-box "Win" "The computer won!")
-       (reset-game))
-
-     ; Verifica si hay un empate después del movimiento del computador
-     (when (draw? game-grid)
-       (message-box "Draw" "It's a draw!")
-       (reset-game))])
-
-  |#
-
-  ; Actualiza la pantalla para mostrar la selección actual
-  (send game-frame refresh)
-  (send this refresh))
 
     ; Forzar el foco en el canvas para la entrada del teclado
     (define/override (on-event e)
@@ -165,6 +134,14 @@
 (define panes (get-panes 0))
 (define canvases (get-panes-canvases panes 0))
 
+#|
+===================================================================
+
+Algunas funciones extras que se necesitan para el juego
+
+===================================================================
+|#
+
 ; Resetea el juego
 (define (reset-game)
   (for [(canvas  canvases)]
@@ -172,6 +149,8 @@
       (send canvas set-color (make-object color% "gray"))
       (send canvas refresh))
   (set! game-grid (get-matrix global-rows global-columns '_))
+  (set! current-row 0) ; Reinicia la posición actual del jugador
+  (set! current-column 0)
   (send game-frame refresh))
 
 ; Define el movimiento del computador
@@ -181,7 +160,18 @@
   (send (list-ref canvases (+ (cadr row-and-column) (* (car row-and-column) global-columns))) set-character #\o)
   (send (list-ref canvases (+ (cadr row-and-column) (* (car row-and-column) global-columns))) refresh))
 
-; Funcion principal
+; Verifica si la celda está vacía
+(define (cell-empty? row col)
+  (equal? (list-ref (list-ref game-grid row) col) '_))
+
+
+#|
+===================================================================
+
+Función principal
+
+===================================================================
+|#
 (define (TTT rows columns)
     (cond 
         ((and (< rows 3) (< columns 3)) (message-box "Error" "The minimum size is 3x3"))
@@ -189,8 +179,6 @@
         (else 
             (set! global-rows rows)
             (set! global-columns columns)
-
-           
             (set! game-frame (new frame% [label "Tic Tac Racket"] [width 600] [height 600]))
             (send game-frame center 'both)
             (set! game-grid (get-matrix global-rows global-columns '_))
@@ -200,4 +188,4 @@
             (send game-frame center 'both)
             (send game-frame show #t))))
 
-(TTT 5 5)
+(TTT 4 4)
